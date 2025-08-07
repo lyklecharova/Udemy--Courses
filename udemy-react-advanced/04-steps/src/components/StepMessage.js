8fa82b5350b8c558c0de784255dba530e9c767c1
lyklecharova <lklecharova9@gmail.com>
2025-08-07 08:06:20 +0300

    Refactor: move and split components into separate files

---
+++ udemy-react-advanced/04-steps/src/components/StepMessage.js
@@ -0,0 +1,9 @@
+
+export function StepMessage({ step, children }) {
+    return (
+        <div className="message">
+            <h3>Step {step}</h3>
+            {children}
+        </div>
+    );
+}
