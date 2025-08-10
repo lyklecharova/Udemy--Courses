b36ecb965b044f9f4d7171132b820d960c3a1335
lyklecharova <lklecharova9@gmail.com>
2025-08-10 14:54:47 +0300

    refactor: pass movies only to relevant components to prevent prop drilling

--- udemy-react-advanced/07-usepopcorn/src/App.js
+++ udemy-react-advanced/07-usepopcorn/src/App.js
@@ -58,7 +58,6 @@ export default function App() {
                 <Box>
                     <MovieList movies={movies} />
                 </Box>
-
                 <Box>
                     <WatchedSummary watched={watched} />
                     <WatchedMoviesList watched={watched} />
@@ -108,41 +107,21 @@ function Main({ children }) {
     return <main className="main">{children}</main>;
 }
 function Box({ children }) {
-    const [isOpen1, setIsOpen1] = useState(true);
+    const [isOpen, setIsOpen] = useState(true);
     return (
         <div className="box">
             <button
                 className="btn-toggle"
-                onClick={() => setIsOpen1((open) => !open)}
+                onClick={() => setIsOpen((open) => !open)}
             >
-                {isOpen1 ? '–' : '+'}
+                {isOpen ? '-' : '+'}
             </button>
-            {isOpen1 && children}
+            {isOpen && children}
         </div>
     );
 }
 
-// function WatchedBox() {
-//     const [watched, setWatched] = useState(tempWatchedData);
-//     const [isOpen2, setIsOpen2] = useState(true);
 
-//     return (
-//         <div className="box">
-//             <button
-//                 className="btn-toggle"
-//                 onClick={() => setIsOpen2((open) => !open)}
-//             >
-//                 {isOpen2 ? '–' : '+'}
-//             </button>
-//             {isOpen2 && (
-//                 <>
-//                     <WatchedSummary watched={watched} />
-//                     <WatchedMoviesList watched={watched} />
-//                 </>
-//             )}
-//         </div>
-//     );
-// }
 
 function MovieList({ movies }) {
     return (
