27c63bebfe7c161aa737cbbcf52acc9d125e6ccb
lyklecharova <lklecharova9@gmail.com>
2025-09-22 16:02:42 +0300

    refactor(index): render App instead of StarRating demo component

--- udemy-react-advanced/07-usepopcorn/src/index.js
+++ udemy-react-advanced/07-usepopcorn/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-//import './index.css';
-//import App from './App';
+import './index.css';
+import App from './App';
 
 import StarRating from './StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <StarRating
+        <App />
+        {/* <StarRating
             maxRating={5}
             messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
         />
-        <StarRating size={24} color="red" className="test" defaultRating={3} />
+        <StarRating size={24} color="red" className="test" defaultRating={3} /> */}
     </React.StrictMode>
 );
